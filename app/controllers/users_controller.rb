cbef5c4e91f717106678177e6e8c198f45659cf5
Jordan <jcharrison12@gmail.com>
2021-06-23 13:45:21 -0500

    Adds happy sad paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email]
+    )
+    if user.save
+      render json: user
+    else
+      render json: {errors: user.errors.full_messages},
+      status: 422
+    end
+  end
+  def update
+    user_id = params[:id]
+    user = User.find(user_id)
+    user.first_name = params[:first_name] || user.first_name
+    user.last_name = params[:last_name] || user.last_name
+    user.email = params[:email] || user.email
+    if user.save
+      render json: user
+    else
+      render json: {errors: user.errors.full_messages}, status: 422
+    end
+  end
 end
